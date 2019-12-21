cda03826153ffd7ab96559c3fae7cdcbd2488412
Jonas Dias <jonasdias@gmail.com>
2019-12-21 16:02:31 -0300

    Improved werbos.py pre-computing the relative frequencies. Corrected some errors in README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Werbos
-Inteligencia Artificial para apoiar a escrita de contos e romances. O nome é uma referência a 
+Inteligência Artificial para apoiar a escrita de contos e romances. O nome é uma referência a 
 [https://en.wikipedia.org/wiki/Paul_Werbos][Paul Werbos], um dos nomes mais importantes na história dos algoritmos 
 que viabilizaram o treinamento de redes neuronais artificiais. Além disso, remete a palavra "verbos", em português.
 
@@ -13,14 +13,14 @@ dos usuários que se interessarem por ele.
 ### Por que esse projeto é útil?
 Eu adoro meu editor de texto predileto. Porém ele não faz tudo o que eu quero. Sei que não estou sozinho. Por isso,
 decidi arregaçar as mangas e programar eu mesmo algumas ferramentas para analisar meus contos e romances. Espero
-que o projeto seja útil para outos escritores também.
+que o projeto seja útil para outros escritores também.
 
 ### Como posso me envolver?
 Se estiver interessado em contribuir com a programação, ficarei muito feliz. Mande um e-mail para 
 **me at jonasdias.com**
 
 ### Onde posso obter mais ajuda, caso necessário?
-Infelizmente ainda não temos uma documentação do projeto. Mas são só meia duzia de linhas python por aqui por enquanto.
+Infelizmente ainda não temos uma documentação do projeto. Mas são só meia dúzia de linhas Python por enquanto.
 
 
 [Paul Werbos]: https://en.wikipedia.org/wiki/Paul_Werbos
--- werbos.py
+++ werbos.py
@@ -1,8 +1,8 @@
 import nltk
 import re
+import matplotlib.pyplot as plt
 
 from matplotlib.colors import rgb2hex
-import matplotlib.pyplot as plt
 from collections import Counter
 
 nltk.download('stopwords')
@@ -10,38 +10,42 @@ nltk.download('punkt')
 
 
 class TextPiece:
-    
-    def __init__(self, text: str, additional_stopwords: list=[]):
+    """
+    A class to analyze a piece of text and get it colored based on word frequency.
+    """
+
+    def __init__(self, text: str, additional_stopwords: list = []):
+        # setup list of stopwords
         stopwords = nltk.corpus.stopwords.words('portuguese')
         stopwords += additional_stopwords
+        # break the text into paragraphs
         paragraphs = text.split("\n")
         # split the text by words        
         self.paragraph_tokens = [nltk.word_tokenize(i) for i in paragraphs]
-        # remove punctuation
+        # remove punctuation and stopwords
         pattern = re.compile('.*[A-Za-z].*')
         words = []
         for tokens in self.paragraph_tokens:
             w = [t.lower() for t in tokens if pattern.match(t) and t not in stopwords]
             words += w
+        # do the counting
         self.counter = Counter(words)
-        
-    def common_words(self, k:int = 10):
+
+    def common_words(self, k: int = 10):
         return self.counter.most_common(k)
-    
+
     def get_html(self, frequency_threshold=1):
         max_counter = max(self.counter.values())
+        rel_counter = {k: v/max_counter for k, v in self.counter.items()}
         # set a colormap
-        # cmap = autumn_r(linspace(0,1,max(max_counter, 5)))
-        # cmap = ListedColormap(cmap[3:,:-1])
         cmap = plt.get_cmap('autumn_r')
         html_tokens = []
         for paragraph in self.paragraph_tokens:
             html_tokens.append('<p>')
             for t in paragraph:
                 # for each token, set a color if it appears more than frequency_threshold in the text
-                if t.lower() in self.counter and self.counter[t.lower()] > frequency_threshold:
-                    relative_count = self.counter[t.lower()] / max_counter
-                    color = rgb2hex(cmap(relative_count))
+                if t.lower() in rel_counter and rel_counter[t.lower()] > frequency_threshold/max_counter:
+                    color = rgb2hex(cmap(rel_counter[t.lower()]))
                     html_tokens.append(f'<font size=4 color={color}>{t}</font>')
                 else:
                     html_tokens.append(t)
