53155e1e1c619157e0a25aec09715460d3a3b42d
Jonas Dias <jonasdias@gmail.com>
2019-12-23 19:14:51 -0300

    Added the barchart with the top 10 word frequencies

--- app.py
+++ app.py
@@ -15,18 +15,23 @@ def index():
 def conta_palavras():
     errors = []
     results = {}
+    words = []
+    frequencies = []
     if request.method == "POST":
         # get text from the box
         try:
             text = request.form['text']
             piece = TextPiece(text)
+            # get top-k words
+            common_words = piece.common_words(k=10)
+            words, frequencies = [i for i, _ in common_words], [j for _, j in common_words]
+            # get colored HTML
             results = piece.get_html()
-            # print(results)
         except Exception as ex:
             errors.append(
-                "Desculpe. Não foi possível processar o texto informado. " + str(ex)
+                "Desculpe. Não foi possível processar o texto informado."  # + str(ex)
             )
-    return render_template('conta_palavras.html', errors=errors, results=results)
+    return render_template('conta_palavras.html', errors=errors, results=results, words=words, frequencies=frequencies)
 
 
 if __name__ == '__main__':
